a44d4fd556b4c48d5f72e39df8b9f8c72ce9e587
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-01-29 16:13:33 -0300

    refactor: meta tag image

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -47,13 +47,13 @@ export default function App({ Component, pageProps }) {
         <meta property="og:url" content="https://metatags.io/" />
         <meta property="og:title" content="007: Quiz" />
         <meta property="og:description" content="Mostre seu conhecimento sobre o agente mais famoso do mundo." />
-        <meta property="og:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="og:image" content="https://i.ytimg.com/vi/SuV05toJoiM/maxresdefault.jpg" />
 
         <meta property="twitter:card" content="summary_large_image" />
         <meta property="twitter:url" content="https://metatags.io/" />
         <meta property="twitter:title" content="007: Quiz" />
         <meta property="twitter:description" content="Mostre seu conhecimento sobre o agente mais famoso do mundo." />
-        <meta property="twitter:image" content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+        <meta property="twitter:image" content="https://i.ytimg.com/vi/SuV05toJoiM/maxresdefault.jpg" />
       </Head>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
